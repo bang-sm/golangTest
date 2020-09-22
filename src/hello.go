0c25093f3a585fa54afc43b3f90f99eaed0e24b1
bang-sm <tjrals1991@gmail.com>
2020-09-22 14:18:18 +0900

    master

--- src/hello.go
+++ src/hello.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("smbang basic 배포")
+	log.Println("starting Web Server") // (1)
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) { // (2)
+		fmt.Fprintln(w, "Hello World!!")
+	})
+
+	err := http.ListenAndServe(":8080", nil) // (3)
+	if err != nil {                          // (4)
+		log.Fatalln(err)
+	}
 }
